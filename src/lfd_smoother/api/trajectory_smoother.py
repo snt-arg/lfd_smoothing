747e879f16781badf8ffa47757d9fac04d8c0b82
abarekatain <alireza.barekatain1996@gmail.com>
2023-07-05 13:24:07 +0200

    fix: generalize trajectory smoother methods

--- src/lfd_smoother/api/trajectory_smoother.py
+++ src/lfd_smoother/api/trajectory_smoother.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pydrake.all import *
 
 from lfd_smoother.core.trajectory_optimizer import TrajectoryOptimizer
@@ -6,21 +8,24 @@ from lfd_smoother.util.demonstration import Demonstration
 from lfd_smoother.util.fr3_drake import FR3Drake
 from lfd_smoother.util.config import FR3Config
 
-import yaml
 
 
 class TrajectorySmoother:
 
     def __init__(self, config):
         self.config = config
-        # with open(config_path, "r") as file:
-        #     self.config = yaml.safe_load(file)
 
-    def run(self, demonstration):
-
+    def read_demo_ros(self, demonstration):
         self.demo = Demonstration()
         self.demo.read_from_ros(demonstration)
         self.demo.filter(thr_translation=self.config["demo_filter_threshold"])
+
+    def read_demo_json(self, filename):
+        self.demo = Demonstration()
+        self.demo.read_from_json(filename=filename)
+        self.demo.filter(thr_translation=self.config["demo_filter_threshold"])
+
+    def run(self):
 
         self.robot = FR3Drake(franka_pkg_xml=self.config["franka_pkg_xml"],
                         urdf_path=self.config["urdf_path"])
@@ -36,5 +41,18 @@ class TrajectorySmoother:
         config.add_initial_guess(initial_guess,timings)
         config.add_solver(IpoptSolver())
         self.smoother = SingleOptimizer(self.robot, config)
-        self.smoother.run()
+        self.result_traj = self.smoother.run()
         
+    def export_raw(self):
+        ts = np.linspace(0, self.result_traj.end_time(), 1000)
+        
+        position = []
+        velocity = []
+        acceleration = []
+        
+        for t in ts:
+            position.append(self.result_traj.value(t))
+            velocity.append(self.result_traj.MakeDerivative().value(t))
+            acceleration.append(self.result_traj.MakeDerivative(2).value(t))
+
+        return ts, np.array(position), np.array(velocity), np.array(acceleration)
