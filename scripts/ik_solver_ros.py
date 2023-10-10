7e207042c9e7107404b468d451b4c99517fee076
abarekatain <alireza.barekatain1996@gmail.com>
2023-10-10 11:10:02 +0200

    feat: ik service and planpose action

--- scripts/ik_solver_ros.py
+++ scripts/ik_solver_ros.py
@@ -3,18 +3,34 @@
 import numpy as np
 import rospy
 
+import actionlib
+from lfd_interface.msg import PlanPoseAction, PlanPoseFeedback, PlanPoseResult, PlanPoseGoal
+from lfd_interface.msg import PlanJointAction, PlanJointGoal
+
 from geometry_msgs.msg import Pose
 from trajectory_msgs.msg import JointTrajectoryPoint
 from lfd_smoothing.srv import IKService, IKServiceResponse
 
 from lfd_smoother.util.ik_solver import IKSolver
 
-
 class IKServer:
-    def __init__(self):
-        # self.service = rospy.Service('ik_service', IKService, self.handle_request)
+    def __init__(self, name):
+        self.ik_flag = False
+        self.service = rospy.Service(name, IKService, self.handle_request)
         ik_solver_config = rospy.get_param("~ik_solver_config")
         self.solver = IKSolver(ik_solver_config)
+    
+    def handle_request(self,req):
+        self.req = req
+        self.ik_flag = True
+        while self.ik_flag:
+            pass
+        q = self.q        
+        return IKServiceResponse(q_out=q)
+    
+    def ik_cb(self):
+        self.q = self.solve_ros(self.req.end_effector_pose, self.req.q_init)
+        self.ik_flag = False
 
     def solve_ros(self, end_effector_pose: Pose,
                     q_init: JointTrajectoryPoint):
@@ -25,7 +41,7 @@ class IKServer:
                         end_effector_pose.orientation.y, end_effector_pose.orientation.z]
         q_init = q_init.positions
 
-        q_values = self.solver.run(position, orientation, q_init)
+        q_values = self.solve(position, orientation, q_init)
         
         q = JointTrajectoryPoint()
         q.positions = q_values
@@ -37,22 +53,60 @@ class IKServer:
         orientation = [w,x,y,z]
         """
         return self.solver.run(position, orientation, q_init)
+
+
+class PlanPoseActionServer:
+    _feedback =  PlanPoseFeedback()
+    _result = PlanPoseResult()
+
+    def __init__(self, name, ik_server):
+        self.action_name = name
+        self.ik_server = ik_server
+        self._as_planpose = actionlib.SimpleActionServer(self.action_name, 
+                                                PlanPoseAction, execute_cb=self.execute_cb, 
+                                                auto_start = False)
+        self._ac_planjoint = actionlib.SimpleActionClient("/plan_joint", PlanJointAction)
+        self._ac_planjoint.wait_for_server()
+        
+        self.ik_flag = False
+        self._as_planpose.start()
     
+    def ik_cb(self):
+        self.q = self.ik_server.solve_ros(self.goal.pose, self.goal.q_init)
+        self.ik_flag = False
+
+    def execute_cb(self, goal : PlanPoseGoal):
+        self.goal = goal
+        self.ik_flag = True
+        while self.ik_flag:
+            pass
+        q = self.q
+        self._feedback.joint_position = q
+        self._as_planpose.publish_feedback(self._feedback)
+
+        goal_planjoint = PlanJointGoal(joint_position=q)
+        self._ac_planjoint.send_goal(goal_planjoint)
+        self._ac_planjoint.wait_for_result()
+
+        success = self._ac_planjoint.get_result().success
+        self._result.success = success
+        self._as_planpose.set_succeeded(self._result)
 
 
 if __name__ == '__main__':
 
     rospy.init_node('ik_solver')
 
-    ik_server = IKServer()
-    print(ik_server.solve([0.303, 0, 0.482], [0,-1,0,0], 
-                    [0.00112954, -0.787257, -0.000108279, -2.36501, 0.00214555, 1.55991, 0.766295])
-    )
+    ik_server = IKServer('/ik_service')
+    planpose = PlanPoseActionServer("/plan_pose", ik_server)
 
-    rospy.spin()
-
-
-
+    r =rospy.Rate(50)
+    while not rospy.is_shutdown():
+        if planpose.ik_flag:
+            planpose.ik_cb()
+        if ik_server.ik_flag:
+            ik_server.ik_cb()
+        r.sleep()
 
 
 
