ec33d1e73d0342e68eed570892cde0478569df0d
abarekatain <alireza.barekatain1996@gmail.com>
2024-05-14 14:53:24 +0200

    fix: set ik initial guess to q0

--- lfd_smoother/lfd_smoother/util/ik_solver.py
+++ lfd_smoother/lfd_smoother/util/ik_solver.py
@@ -54,13 +54,16 @@ class IKSolver:
                                                 self.robot.plant.world_frame(), pose.rotation(), 0)
         
         self.prog = self.ik_solver.get_mutable_prog()
-        self.prog.AddCost((self.ik_solver.q()-q0).dot(self.ik_solver.q()-q0))
+        self.prog.AddCost(((self.ik_solver.q()-q0).dot(self.ik_solver.q()-q0)) * 1e-2)
+        self.prog.SetInitialGuess(self.ik_solver.q(), q0)
+
 
         solver = IpoptSolver()
 
         # result = Solve(self.prog)
         result = solver.Solve(self.prog)
         q = result.GetSolution(self.ik_solver.q())
+        print('optimal cost = ', result.get_optimal_cost())
         self.visualize(q)
         return q[:num_q]
 
